6fb5edd2055c83e4f7070782a97405cbb281f74e
Tanvie <tanvieki@buffalo.edu>
2019-04-02 18:13:48 -0400

    added pom.xml -- trying to make json in server work

--- server.py
+++ server.py
@@ -2,21 +2,23 @@ import bottle
 import json
 import users
 
-
 @bottle.route("/")
 def any_name():
-    return bottle.static_file("FrontEnd.html", root="")
+    return bottle.static_file("FrontEnd.html", root="FrontEnd.html")
+
 
 @bottle.route("/game.js")
 def any_name():
-    return bottle.static_file("game.js", root="")
+    return bottle.static_file("game.js", root="game.js")
+
 
 @bottle.post('/Login')
 def register_user():
     content = bottle.request.body.read().decode()
     content = json.loads(content)
-    users.update_users(content)
+    users.update_users(content["username"])
     return json.dumps(users.view_users())
 
 
-bottle.run(host="0.0.0.0", port=8080, debug=True)
+if __name__ == '__main__':
+    bottle.run(host="0.0.0.0", port=8080, debug=True)
